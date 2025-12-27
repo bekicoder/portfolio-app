ec2bb085aa2041fad51b2c13009a707e1392e4ce
bekicoder <bereketgrima123@gmail.com>
2025-12-27 15:41:11 +0300

    fixed moon image path

--- src/components/moon.jsx
+++ src/components/moon.jsx
@@ -3,7 +3,7 @@ function MoonMessage() {
         <div className="text-white overflow-clip px-3 text-center flex flex-col items-center pt-28" >
             <h1 className="text-4xl mb-5">Let's <span className="text-[#1fff00]">Vist</span> Moon</h1>
             <p>In the next few years, my goal is to advance my coding skills to an exceptional level—aiming as high as the moon.</p>
-            <img src="./public/moon_PNG51.png" className="rounded-full w-full max-w-xs flex-1 moonAnimate -z-10" />
+            <img src="/moon_PNG51.png" className="rounded-full w-full max-w-xs flex-1 moonAnimate -z-10" />
         </div >
 
     )
